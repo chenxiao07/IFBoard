73d8b1b21dfcbc838c4a5b46068d94effaaf8ddc
chen-xiao <chen-xiao@MacmatoMacBook-Air.local>
2012-11-14 21:47:30 +0900

    second commit || changed to socket.io

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
 "use strict";
 
-var webSocketsServerPort = 1337;
-
-var webSocketServer = require('websocket').server;
+var io = require('socket.io');
 var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
 //var redis = require("redis").createClient();
 var exec = require("child_process").exec;
-var fs = require("fs");
-var url = require('url');
-var path = require("path");
-var mime = require("mime");
 
-var clients = [];
-var users = [];
-var map = [];
-var map_width = 34;
-var map_height = 34;
+
 
 var server = http.createServer(function(request, response) {
     var uri = url.parse(request.url).pathname;
@@ -44,24 +38,23 @@ var server = http.createServer(function(request, response) {
             response.end();
         });
     });
-}).listen(webSocketsServerPort);
+}).listen(1337);
 
-/**
- * WebSocket server
- */
-var wsServer = new webSocketServer({
-    // WebSocket server is tied to a HTTP server. WebSocket request is just
-    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
-    httpServer: server
-});
 
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
-wsServer.on('request', function(request) {
-    var connection = request.accept(null, request.origin);
+
+io = io.listen(server);
+
+
+var users = [];
+var map = [];
+var map_width = 34;
+var map_height = 34;
+
+io.sockets.on('connection', function (socket) {
+    socket.emit("ready", 1);
 
     for (var i=0; i<users.length; i++) {
-        connection.sendUTF(JSON.stringify(users[i]));
+        if(users[i]) socket.emit("join", JSON.stringify(users[i]));
     }
 
     var r = parseInt(Math.random()*255);
@@ -71,7 +64,7 @@ wsServer.on('request', function(request) {
     var msg = {};
     msg.type = "join";
     msg.data = {};
-    msg.data.id = "cursor"+clients.length;
+    msg.data.id = "cursor"+users.length;
     msg.data.r = r;
     msg.data.g = g;
     msg.data.b = b;
@@ -79,22 +72,28 @@ wsServer.on('request', function(request) {
     msg.data.y = 0;
     var json = JSON.stringify(msg);
 
-    for (var i=0; i < clients.length; i++) {
-        if(clients[i]) clients[i].sendUTF(json);
-    }
+    socket.broadcast.emit("join", json);
 
-    var index = clients.push(connection) - 1;
-    users.push(msg);
+    var index = users.push(msg) - 1;
 
-    connection.on('message', function(message) {
-        try {
-            var message = JSON.parse(message.utf8Data);
-        } catch (e) {
-            console.log('This doesn\'t look like a valid JSON: ', message);
-            return;
-        }
+    socket.on('get_map', function (data) {
+        var message = JSON.parse(data);
+            var x = message.data.x;
+            var y = message.data.y;
+            if(map["mapx"+x+"mapy"+y]) {
+                message.data.content = map["mapx"+x+"mapy"+y];
+                socket.emit("get_map", JSON.stringify(message));
+            } else {
+                exec("perl maze.pl", function(error, stdout, stderr) {
+                    map["mapx"+x+"mapy"+y] = stdout;
+                    message.data.content = stdout;
+                    socket.emit("get_map", JSON.stringify(message));
+                });
+            }
+    });
 
-        if (message.type === 'type_in') {
+    socket.on('type_in', function (data) {
+        var message = JSON.parse(data);
             users[index].data.x = message.data.x;
             users[index].data.y = message.data.y;
             var s_x = parseInt(message.data.x/map_width);
@@ -103,53 +102,37 @@ wsServer.on('request', function(request) {
             var y = message.data.y%map_height;
             map["mapx"+s_x+"mapy"+s_y] = map["mapx"+s_x+"mapy"+s_y].substring(0,y*map_width+x)+message.data.value+map["mapx"+s_x+"mapy"+s_y].substring(y*map_width+x+1,map["mapx"+s_x+"mapy"+s_y].length);
             message.data.id = "cursor"+index;
-            var json = JSON.stringify(message);
-            for (var i=0; i < clients.length; i++) {
-                if(i!=index && clients[i]) {
-                    clients[i].sendUTF(json);
-                }
-            }
+            socket.broadcast.emit("type_in", JSON.stringify(message));
+    });
 
-        } else if(message.type === "move") {
+    socket.on('delete', function (data) {
+        var message = JSON.parse(data);
+            users[index].data.x = message.data.x;
+            users[index].data.y = message.data.y;
+            var s_x = parseInt(message.data.x/map_width);
+            var s_y = parseInt(message.data.y/map_height);
+            var x = message.data.x%map_width;
+            var y = message.data.y%map_height;
+            map["mapx"+s_x+"mapy"+s_y] = map["mapx"+s_x+"mapy"+s_y].substring(0,y*map_width+x)+" "+map["mapx"+s_x+"mapy"+s_y].substring(y*map_width+x+1,map["mapx"+s_x+"mapy"+s_y].length);
+            message.data.id = "cursor"+index;
+            socket.broadcast.emit("delete", JSON.stringify(message));
+    });
+
+    socket.on('move', function (data) {
+        var message = JSON.parse(data);
             users[index].data.x = message.data.x;
             users[index].data.y = message.data.y;
             message.data.id = "cursor"+index;
-            var json = JSON.stringify(message);
-            for (var i=0; i < clients.length; i++) {
-                if(i!=index && clients[i]) {
-                    clients[i].sendUTF(json);
-                }
-            }
-        } else if(message.type === "get_map") {
-            var x = message.data.x;
-            var y = message.data.y;
-            if(map["mapx"+x+"mapy"+y]) {
-                message.data.content = map["mapx"+x+"mapy"+y];
-                clients[index].sendUTF(JSON.stringify(message));
-            } else {
-                exec("perl maze.pl", function(error, stdout, stderr) {
-                    map["mapx"+x+"mapy"+y] = stdout;
-                    message.data.content = stdout;
-                    clients[index].sendUTF(JSON.stringify(message));
-                });
-            }
-        }
+            socket.broadcast.emit("move", JSON.stringify(message));
     });
 
-    // user disconnected
-    connection.on('close', function(connection) {
-        clients[index] = 0;
+    socket.on('disconnect', function () {
         users[index] = 0;
 
         var msg = {};
         msg.type = "exit";
         msg.data = {};
         msg.data.id = "cursor"+index;
-        var json = JSON.stringify(msg);
-
-        for (var i=0; i < clients.length; i++) {
-            if(clients[i]) clients[i].sendUTF(json);
-        }
-    });
-
+        socket.broadcast.emit("exit", JSON.stringify(msg));
+    })
 });
